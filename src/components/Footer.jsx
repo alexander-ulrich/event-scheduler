f51358b7d549205db5b7ba23ffedef5ad3384062
Alexander Ulrich <alexander.ulrich@ymail.com>
2025-10-13 17:03:30 +0200

    add Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div>
+      <h2 className="text-center fixed bottom-5 left-[50%] translate-x-[-50%]">
+        &copy; 2025 Event Scheduler by Sven and Alexander
+      </h2>
+    </div>
+  );
 }
