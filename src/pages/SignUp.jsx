4bfb79b26fb58cfd8f3d993241284eb7c25e6df8
Alexander Ulrich <alexander.ulrich@ymail.com>
2025-10-13 18:26:58 +0200

    add Register new user functionality and clean up sign in page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,3 +1,97 @@
+import { useNavigate } from "react-router";
+import { useEffect, useRef, useState } from "react";
+import { registerRequest } from "../utils/apiAccess";
+
 export default function SignUp() {
-  return <div>Sign Up</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [registerResult, setRegisterResult] = useState({
+    credentials: null,
+    error: null,
+    success: false,
+  });
+  const pwRef = useRef();
+  const navigate = useNavigate();
+
+  //add new User to API database
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const requestBody = { email: email, password: password };
+      const res = await registerRequest(requestBody);
+      setRegisterResult(res);
+    } catch (error) {
+      console.log(error.message);
+      setRegisterResult(res);
+    } finally {
+      //  State zurücksetzen
+      setLoading(false);
+      setPassword("");
+      pwRef.current.value = "";
+    }
+  }
+  //Redirect to login page on successful registrtion for authentication
+  useEffect(() => {
+    if (registerResult.success) navigate("/sign-in");
+  }, [registerResult.success]);
+
+  return (
+    <div className="flex flex-col items-center">
+      <form onSubmit={handleSubmit}>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Register</legend>
+
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+            disabled={loading}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            ref={pwRef}
+            className="input"
+            placeholder="Password"
+            disabled={loading}
+            autoComplete="new-password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          {registerResult?.error && (
+            <p className="text-red-600 mt-1 font-semibold text-center">
+              {registerResult?.error}
+            </p>
+          )}
+          {registerResult?.success && (
+            <p className="text-green-600 mt-1 font-semibold text-center">
+              Registration complete! Redirecting...
+            </p>
+          )}
+          <button
+            type="submit"
+            disabled={loading}
+            className="btn btn-neutral mt-4"
+          >
+            {loading ? "Submitting..." : "Register"}
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
--- src/utils/apiAccess.js
+++ src/utils/apiAccess.js
@@ -6,6 +6,7 @@ export async function loginRequest(loginCredentials) {
   const authUrl = "/api/auth/login";
   const result = { credentials: null, error: null, success: null };
   try {
+    await sleep(2000);
     const response = await fetch(baseUrl + authUrl, {
       method: "POST",
       headers: {
@@ -35,3 +36,40 @@ export async function loginRequest(loginCredentials) {
     return result;
   }
 }
+
+export async function registerRequest(registerCredentials) {
+  const registerUrl = "/api/users";
+  const result = { user: null, error: null, success: null };
+  try {
+    await sleep(2000);
+    const response = await fetch(baseUrl + registerUrl, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registerCredentials),
+    });
+    if (!response.ok) {
+      result.error = "Something went wrong. Please try again!";
+      result.success = false;
+      throw new Error("Something went wrong. Please try again!");
+    }
+    console.log("Authentication complete!");
+
+    const data = await response.json();
+    // saveToLocalStorage("user", data);
+    result.user = data;
+    result.success = true;
+  } catch (error) {
+    console.log(error.message);
+    result.success = false;
+    result.error = error.message;
+  } finally {
+    console.log(result);
+
+    return result;
+  }
+}
+
+export const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
